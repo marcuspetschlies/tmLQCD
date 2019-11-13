0e354332593a930163bb301d00f9c256d0174874
Marcus <marcus.petschlies@web.de>
2019-11-13 14:18:43 +0100

    static inline; commented double defs of functions

--- buffers/utils.h
+++ buffers/utils.h
@@ -1,25 +1,29 @@
+#ifndef _BUFFERS_UTILS_H
+#define _BUFFERS_UTILS_H
+
 #pragma once
 
 #include <buffers/gauge.h>
 
-void copy_gauge_field(gauge_field_t dest, gauge_field_t orig);
-
 void generic_exchange(void *field_in, int bytes_per_site);
-void exchange_gauge_field(gauge_field_t target);
-void exchange_gauge_field_array(gauge_field_array_t target);
+// void copy_gauge_field(gauge_field_t dest, gauge_field_t orig);
+// void exchange_gauge_field(gauge_field_t target);
+// void exchange_gauge_field_array(gauge_field_array_t target);
 
-inline void copy_gauge_field(gauge_field_t dest, gauge_field_t orig)
+static inline void copy_gauge_field(gauge_field_t dest, gauge_field_t orig)
 {
   memmove((void*)dest.field, (void*)orig.field, sizeof(su3_tuple) * VOLUMEPLUSRAND + 1);
 }
 
-inline void exchange_gauge_field(gauge_field_t target)
+static inline void exchange_gauge_field(gauge_field_t target)
 {
   generic_exchange((void*)target.field, sizeof(su3_tuple));
 }
 
-inline void exchange_gauge_field_array(gauge_field_array_t target)
+static inline void exchange_gauge_field_array(gauge_field_array_t target)
 {
   for (unsigned int idx = 0; idx < target.length; ++idx)
     exchange_gauge_field(target.field_array[idx]);
 }
+
+#endif
