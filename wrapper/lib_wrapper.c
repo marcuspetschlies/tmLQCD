da772e7d87d8bc16bde6a33e3a5f75e5ee23454d
Marcus <marcus.petschlies@web.de>
2023-03-01 12:52:49 +0100

    set communicator by duplication

--- wrapper/lib_wrapper.c
+++ wrapper/lib_wrapper.c
@@ -403,7 +403,7 @@ int tmLQCD_get_mpi_params(tmLQCD_mpi_params* params) {
   params->proc_coords[3] = g_proc_coords[3];
   // g_cart_grid only exists when MPI is in use
 #ifdef TM_USE_MPI
-  params->cart_grid = g_cart_grid;
+  MPI_Comm_dup(g_cart_grid, &(params->cart_grid) );
 #else
   params->cart_grid = -1;
 #endif
